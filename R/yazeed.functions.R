79596ef3a3efa120f19ad3802c83857c6668d871
Yazeed Alsuhaibany <yazeed771@hotmail.com>
2018-01-11 20:20:57 -0800

    adding 3 functions

---
+++ R/yazeed.functions.R
@@ -0,0 +1,14 @@
+
+
+yazeed <- function(){
+  print('Hello, I\'m Yazeed\'s function')
+}
+
+c.to.f <- function (c.temp) {
+  c.temp * 1.8 + 32
+}
+
+f.to.c <- function (f.temp) {
+  (f.temp - 32) * 0.5556
+}
+
